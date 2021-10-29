b21ad89b3af60c96b02c04e1ebc816cfc9ad8963
Tio Marco (TioDarius1) <91704772+TioMarc0@users.noreply.github.com>
2021-10-29 10:59:36 -0300

    Initial commit

---
+++ funcaoEx4.php
@@ -0,0 +1,6 @@
+<?php
+$soma = 10;
+$sub = 6;
+$multi = 9;
+$div = 36;
+?>
---
+++ index.php
@@ -0,0 +1,17 @@
+
+<?php
+include 'funcaoEx4.php';
+
+
+$soma=10;
+echo $soma. "<br>";
+echo $sub. "<br>";
+echo $multi. "<br>";
+echo $div. "<br>";
+
+echo soma(3);
+
+function soma($n1){
+    return $n1 + $GLOBALS["soma"];
+}
+?>
